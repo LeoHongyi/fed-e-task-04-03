caae6cab0360fbc204643fab5124fb6b23e563b4
wenhualiuliu <wenhualiu.liu@gmail.com>
2021-03-30 21:25:02 +0800

    init

---
+++ src/api/login.js
@@ -0,0 +1,12 @@
+
+
+
+import request from './'
+
+export const signUp = (params) => {
+    return request.post('/users', { user: params })
+}
+
+export const signIn = (params) => {
+    return request.post('/users/login', { user: params })
+}
